3dcca6a5533e029f05e72d97cff52f8733a0e068
Danilo Batista <dbatistadsn@gmail.com>
2022-08-23 18:00:43 -0300

    feat: Challenge completed.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,7 @@
 "use strict";
 
+/* Year in the header */
+const fullDate = document.getElementById("header__fulldate").innerHTML = new Date().toLocaleString();
+
 /* Year in the footer */
 const year = document.getElementById("year").innerHTML = new Date().getFullYear();
